2bd3ca7cb051801e5395a57985a566b51b93d5af
Harry Pantaleon <pantaleonhp@gmail.com>
2023-01-11 22:24:57 +0800

    Initial Commit

---
+++ BinaryBoardPuzzle/BinaryBoardPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryBoardPuzzleApp.swift
+//  BinaryBoardPuzzle
+//
+//  Created by Harry Pantaleon on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BinaryBoardPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
